16968063dd90c535c272d03b5f6f0ef246aa4c8e
Lukas Zeller <luz@plan44.ch>
2016-12-10 13:13:05 +0100

    Initial Commit: empty project with p44utils included and building in XCode

---
+++ src/pixelboardd_main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  pixelboardd
+//
+//  Created by Lukas Zeller on 10.12.16.
+//  Copyright © 2016 plan44.ch. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+    return 0;
+}
